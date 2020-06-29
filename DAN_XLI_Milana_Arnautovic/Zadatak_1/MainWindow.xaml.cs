9ad8e4af44a75cbf52460d3947ebe3a0ef6522e2
Milana8 <91amilana@gmail.com>
2020-06-29 19:09:24 +0200

    Adding methods: worker_ProgressChanged, worker_DoWork, worker_RunWorkerCompleted

--- DAN_XLI_Milana_Arnautovic/Zadatak_1/MainWindow.xaml.cs
+++ DAN_XLI_Milana_Arnautovic/Zadatak_1/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +23,87 @@ namespace Zadatak_1
     /// </summary>
     public partial class MainWindow : Window
     {
+        static BackgroundWorker worker = new BackgroundWorker();
+
+        string number { get; set; }
+        string print { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            worker.DoWork += worker_DoWork;
+            worker.ProgressChanged += worker_ProgressChanged;
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+
+        }
+
+        private void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pbStatus.Value = e.ProgressPercentage;
+            Percentage.Content = e.ProgressPercentage.ToString() + "%";
+        }
+
+
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            bool isValid = int.TryParse(number, out int numOfCopies);
+
+
+            int percentage;
+            int sum = 0;
+
+
+            for (int i = 1; i <= numOfCopies; i++)
+            {
+                Thread.Sleep(1000);
+                percentage = 100 / numOfCopies;
+                sum = sum + percentage;
+                worker.ReportProgress(sum);
+
+                if (worker.CancellationPending)
+                {
+                    e.Cancel = true;
+
+                    worker.ReportProgress(0);
+                    return;
+                }
+
+
+                string path = "../../" + i + "." + DateTime.Now.Day + "_" + DateTime.Now.Month + "_" + DateTime.Now.Year + "_" + DateTime.Now.Hour + "_" + DateTime.Now.Minute + ".txt";
+
+
+                File.WriteAllText(path, print);
+            }
+
+            e.Result = sum;
+        }
+
+        private void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                Message.Content = "Processing cancelled";
+            }
+            else if (e.Error != null)
+            {
+                Message.Content = e.Error.Message;
+            }
+            else
+            {
+                Message.Content = e.Result.ToString();
+            }
+        }
+
+        private void Printing_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
